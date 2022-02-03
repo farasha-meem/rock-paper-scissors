e2082b2e5b20fe3b3f6005eebb678ef1134e6007
Farasha Rahman Meem <farasha.rahman@gmail.com>
2022-02-02 23:27:23 -0500

    ui-changes

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let compScore = 0;
+
 function computerPlay() {
     let options = ["rock", "paper", "scissors"];
     let optionNum = Math.floor(Math.random() * (2 + 1));
@@ -8,39 +11,56 @@ function computerPlay() {
 function playRound(playerSelection) {
     playerSelection = playerSelection.toLowerCase();
     let computerSelection = computerPlay();
-    let computerString = `Computer's hand was ${computerSelection}.`;
+    function returnAction(result) {
+        if (result == "win") {
+            playerScore += 1;
+        }
+        else if (result == "lose") {
+            compScore += 1;
+        }
+        return `Computer's hand was ${computerSelection}. You ${result}! Player Score: ${playerScore}. Computer Score: ${compScore}`;
+    }
     if (playerSelection === computerSelection) {
-        return computerString + "It's a tie!";
+        returnAction("tie");
     }
     if (playerSelection === "rock") {
         if (computerSelection === "scissors") {
-            return computerString + "You win!";
+            return returnAction("win");
         }
         else {
-            return computerString + "You lose!";
+            return returnAction("lose");
         }
     }
     else if (playerSelection === "paper") {
         if (computerSelection === "scissors") {
-            return computerString + "You lose!";
+            return returnAction("lose");
         }
         else {
-            return computerString + "You win!";
+            return returnAction("win");
         }
     }
     else {
         if (computerSelection === "rock") {
-            return computerString + "You lose!";
+            return returnAction("lose");
         }
         else {
-            return computerString + "You win!";
+            return returnAction("win");
         }
     }
 }
 
-function game() {
-    for (i = 0; i < 5; i++) {
-        let playerSelection = window.prompt(`Play your hand for round ${i + 1}`);
-        alert(playRound(playerSelection));
+function game(playerSelection) {
+    let resultsDiv = document.getElementById("results");
+    resultsDiv.innerText = playRound(playerSelection);
+    function announceWinner(text) {
+        alert(text);
+        playerScore = 0;
+        compScore = 0
+    }
+    if (playerScore == 5) {
+        announceWinner("WINNER!!!!! :D :D :D");
+    }
+    if (compScore == 5) {
+        announceWinner("LOSER :( :( :(");
     }
 }
